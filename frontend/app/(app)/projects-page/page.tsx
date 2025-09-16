286201653b402b0d330caa7b44472b0b35608db6
zander37 <alexander.dalton@ufl.edu>
2025-09-16 14:02:52 -0400

    Created Boilerplate ProjectCard Component

--- frontend/app/(app)/projects-page/page.tsx
+++ frontend/app/(app)/projects-page/page.tsx
@@ -1,5 +1,12 @@
 // app/(app)/projects-page/page.tsx
+import ProjectCard from "@/components/ProjectCard";
+
 export default function ProjectsPage() {
-    return <div style={{padding: 24}}>Projects list goes here</div>;
+    return <>
+      <div style={{padding: 24}}>Projects list goes here</div>
+      <ProjectCard name="Project 1" description="Project 1 Desc." labeledCount={4} totalImages={2} lastLabeledAt="9/14/2025"/>
+      <ProjectCard name="Project 2" description="Project 2 Desc." labeledCount={9} totalImages={3} lastLabeledAt="9/15/2025"/>
+      <ProjectCard name="Project 3" labeledCount={1} totalImages={2} lastLabeledAt=""/>
+    </>;
   }
   
