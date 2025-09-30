a77627b1dbd3cea4767d8153453cb2f4044071ae
Jossaya Camille <jossayacamille@gmail.com>
2025-09-30 01:02:37 -0400

    Completed project card styling and impl

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,14 +1,16 @@
 import "./globals.css";
+import { Montserrat } from 'next/font/google'
 export const metadata = {
     title: "Labeler MVP",
     description: "Next.js + Konva + OpenCV.js"
   };
 
-  
+  const geist = Montserrat({ subsets: ['latin']})
+ 
   export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-      <html lang="en">
-        <body style={{ margin: 0, fontFamily: "Inter, system-ui, Arial" }}>
+      <html lang="en" className={geist.className}>
+        <body style={{ margin: 0 }}>
           {children}
         </body>
       </html>
--- frontend/components/ProjectCard.tsx
+++ frontend/components/ProjectCard.tsx
@@ -37,54 +37,63 @@ function ProjectCard({
     total: number,
      } ) 
 {   
-    const [hover, setHover] = useState<{x: number, y: number} | false>(false);
+    const [hover, setHover] = useState<boolean>(false);
     const dateRef = useRef<HTMLDivElement>(null);
-    const tooltipRef = useRef<HTMLDivElement>(null);
     const timePassed = TimePassed(lastLabeledAt)
     useEffect(() => {
         const onHover = (e: MouseEvent) => {
-            setHover({x: e.offsetX, y: e.offsetY});
+            setHover(true);
         };
-        const onLeave = () => {
+        const onLeave = (e) => {
             setHover(false);
             if (dateRef.current) dateRef.current.removeEventListener('mouseover',onHover);
         }
         if (dateRef.current) {
-            dateRef.current.addEventListener('mouseover', onHover);
+            dateRef.current.addEventListener('mousemove', onHover);
             dateRef.current.addEventListener('mouseleave', onLeave)
         }
 
 
         return () => {
-            if (dateRef.current) dateRef.current.removeEventListener('mouseover',onHover);
+            if (dateRef.current) {
+                dateRef.current.removeEventListener('mousemove', onHover);
+                dateRef.current.removeEventListener('mouseleave', onLeave)
+            }
         }
     });
-    useEffect(() => {
-        if (hover && tooltipRef.current) {
-            tooltipRef.current.style.left = `${hover.x}px`;
-            tooltipRef.current.style.top = `${hover.y}px`;
-        } else if (tooltipRef.current)  {
-            tooltipRef.current.style.left = '';
-            tooltipRef.current.style.top = '';
-        }
-    }, [hover])
-    const Card = (<div className="hover:scale-105 hover:cursor-pointer duration-250 border-4 rounded-lg p-6 min-h-fit min-w-[247px] w-100 h-50 flex-col relative" style={{backgroundColor: indexToColor(index, total)}} onClick={onClick}>
-        <h2 className="text-3xl font-bold underline">{name}</h2>
-        { description && <p>{description}</p> }
+   
+    const Card = (<div className="hover:-translate-y-1 hover:cursor-pointer hover:shadow-2xl duration-250 shadow-xl rounded-lg px-6 py-2 min-h-fit min-w-[247px] w-100 h-50 flex-col relative" style={{backgroundColor: indexToColor(index, total)}} onClick={onClick}>
+        <div className="flex flex-row items-center justify-start">
+            <h2 className="text-2xl line-clamp-1">{name}</h2>
+            <span className="ml-auto -translate-y-3 translate-x-1.5 text-4xl  ">…</span>
+
+        </div>
+        <div className="w-full h-[1px] opacity-25 my-0.5 bg-black"></div>
+        { description && <p className="line-clamp-4 text-sm">{description}</p> }
         
-        <p>Last Labeled: {lastLabeledAt.toLocaleDateString() ?? "-"}</p>
-        <div ref={dateRef} className="bg-gray-500 bg-w-full px-6 w-full py-3 absolute bottom-0 left-0 flex flex-row">
-            <p className="text-white">{timePassed}</p>
-            <p className="font-bold ml-auto "
-                style={{color: `hsl(${labeledCount * 120 / totalImages}, 100%, 50%)`}}>
-                    {labeledCount} / {totalImages}
-            </p>
+        <div ref={dateRef} className="bg-gray-500 bg-w-full px-6 rounded-b-lg w-full h-[30%] absolute bottom-0 left-0 flex flex-row items-center">
+            {hover ? (
+                <>
+                <p className="text-white">{lastLabeledAt.toLocaleDateString()}</p>
+                <p className="text-white ml-auto" 
+                    style={{color: `hsl(${labeledCount * 120 / totalImages}, 100%, 50%)`}} >
+                        {labeledCount * 100 / totalImages}% completed
+                    </p>
+
+                </>
+            ) : (
+                <>
+                    <p className="text-white">{timePassed}</p>
+                    <p className="font-bold ml-auto "
+                        style={{color: `hsl(${labeledCount * 120 / totalImages}, 100%, 50%)`}}>
+                            {labeledCount}/{totalImages}
+                    </p>
+                </>
+            )}
 
         </div>
         
-        <div ref={tooltipRef} className={`bg-gray-500 bg-w-full px-6 w-full py-3 absolute bottom-0 left-0 ${hover ? 'fixed' : 'hidden'}`}>
-
-        </div>
+        
     </div>);
 
     return href ? <a href={href}>{Card}</a> : Card;
